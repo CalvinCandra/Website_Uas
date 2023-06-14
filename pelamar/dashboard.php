be28f78a22f52feea570042a7408ff9f604cacdb
IGLOW2004 <gungwiyana@gmail.com>
2023-06-14 14:16:02 +0800

    Oka Wiyana

--- pelamar/dashboard.php
+++ pelamar/dashboard.php
@@ -1,1 +1,53 @@
-<h1>Halaman Pelamar</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- link css bootstrap -->
+    <link rel="stylesheet" href="../bootsraps/css/bootstrap.css">
+
+    <!-- link css -->
+    <link rel="stylesheet" href="bootsraps/styeling.css">
+
+    <!-- link font Awsome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"> 
+    <title>Bali Job Finder</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #2B1B56;">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="../bootsraps/img/BJF_sb.png" alt="Bootstrap" width="150px">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0 me-3">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#">Tips</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control rounded-pill" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success rounded-pill" type="submit"><i class="fa fa-search"></i></button>
+        <span class="navbar-text ms-3 me-3">
+          <i class="fa fa-user"></i>
+        </span>
+      </form>
+    </div>
+  </div>
+</nav>
+
+
+
+
+<script src="bootsraps/js/bootstrap.js"></script>
+</body>
+</html>
