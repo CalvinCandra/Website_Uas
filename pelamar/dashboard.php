237d96ddc0dd734286aaa10ee0e229c308b88fbe
IGLOW2004 <gungwiyana@gmail.com>
2023-06-16 22:30:15 +0800

    oka wiyana

--- pelamar/dashboard.php
+++ pelamar/dashboard.php
@@ -3,51 +3,59 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- link css bootstrap -->
-    <link rel="stylesheet" href="../bootsraps/css/bootstrap.css">
-
-    <!-- link css -->
-    <link rel="stylesheet" href="bootsraps/styeling.css">
-
-    <!-- link font Awsome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"> 
     <title>Bali Job Finder</title>
+    <!-- file Css -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"> 
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #2B1B56;">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">
-      <img src="../bootsraps/img/BJF_sb.png" alt="Bootstrap" width="150px">
-    </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarText">
-      <ul class="navbar-nav ms-auto mb-2 mb-lg-0 me-3">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="#">About</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="#">Tips</a>
-        </li>
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control rounded-pill" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success rounded-pill" type="submit"><i class="fa fa-search"></i></button>
-        <span class="navbar-text ms-3 me-3">
-          <i class="fa fa-user"></i>
-        </span>
-      </form>
-    </div>
-  </div>
-</nav>
+    <nav class="navbar navbar-expand-lg navbar-dark shadow-lg fixed-top" style="background-color: #2B1B56;">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            <img src="../bootsraps/img/BJF_sb.png" alt="Bali Job Finder" height="40px">
+          </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse text-right" id="navbarText">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link" href="#Home">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#About">About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#Tips">Tips</a>
+              </li>
+              <li>
+                <form class="d-flex me-3" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success rounded-circle" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+                  </form>
+              </li>
+              <li>
+                <i class="fa-solid fa-user-tie fa-2x" style="color: #ffffff;"></i>
+              </li>
+            </ul>
 
+          </div>
+        </div>
+      </nav>
+      <div class="container-fluid banner">
+        <div class="container  teat-left"></div>
+        <h4 class="display-6">Welcome</h4>
+        <h3 class="display-1">Bali Job Finder</h3>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora voluptates corrupti quia possimus, sit beatae?</p>
+      </div>
+      <div class="container-fluid tips">
+        <div class="container text-right">
+          <h2 class="display-3">Tips Melamar</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, ducimus, illum aperiam neque ea perferendis, laudantium dolorem maxime cumque aut nobis dolores iusto recusandae delectus?</p>
+        </div>
+      </div>
 
-
-
-<script src="bootsraps/js/bootstrap.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js" integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous"></script>
 </body>
 </html>
