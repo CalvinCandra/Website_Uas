56baadc485357b74c1ff0790b8f6d2fbc0986918
IGLOW2004 <gungwiyana@gmail.com>
2023-06-20 20:46:50 +0800

    perbaikan tampilan

--- pelamar/dashboard.php
+++ pelamar/dashboard.php
@@ -7,147 +7,156 @@
     <!-- file Css -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"> 
-    <link rel="stylesheet" href="dashStyle.css">
+    <link rel="stylesheet" href="Style.css">
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark shadow-lg fixed-top" style="background-color: #2B1B56;">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">
-            <img src="../bootsraps/img/BJF_sb.png" alt="Bali Job Finder" class="logo-img">
-          </a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse text-right" id="navbarText">
-            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link" href="#Home">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#About">About</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#tips">Tips</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#tips">Lowongan</a>
-              </li>
-              <li>
-                <i class="fa-solid fa-user-tie fa-2x" style="color: #ffffff;"></i>
-              </li>
-            </ul>
-
-          </div>
-        </div>
-      </nav>
-
-      <div class="container-fluid banner text-left text ">
-        <div class="container"></div>
-        <h4 class="display-6">Welcome</h4>
-        <h3 class="display-1">Bali Job Finder</h3>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora voluptates corrupti quia possimus, sit beatae?</p>
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg navbar-dark shadow-lg fixed-top" style="background-color: #2B1B56;">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="../bootsraps/img/BJF_sb.png" alt="Bali Job Finder" height="40px">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+      <div class="collapse navbar-collapse text-right" id="navbarText">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" href="#Home">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#About">About</a>
+          </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#Tips">Tips</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="Lowongan">Lowongan</a>
+            </li>
+           <li>
+            <i class="fa-solid fa-user-tie" style="color: #ffffff;"></i>
+          </li>
+        </ul>
       </div>
-
-      <div class="container-fluid tips" id="tips">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-6">
-                    <a href="#" class="logo">
-                        <img src="../bootsraps/img/logotips.png" alt="" width="500px">
-                    </a>
-                </div>
-                <div class="col-md-6">
-                    <h3 class="display-4 text-center">Tips Melamar</h3>
-                    <p class="display-8 ">Berikut adalah tips singkat untuk melamar pekerjaan dari kami. Riset dan persiapkan diri dengan baik, perbarui resume dan surat lamaran sesuai dengan pekerjaan yang dilamar, tampilkan keahlian dan portofolio yang relevan, persiapkan diri dengan baik untuk wawancara, dan tunjukkan sikap profesional sepanjang proses melamar. Dengan menerapkan tips-tips ini, Anda dapat meningkatkan peluang sukses dalam melamar pekerjaan.</p>
-                </div>
-            </div>
-        </div>
-    </div>
-
-
-       <!-- logo start -->
-<div class="bungkus">
-  <div class="conten teks-center">
-    <h1>Rekomendasi Perusahaan Dari Kami</h1>
   </div>
-    <div class="mahasiswa">
-        <div class="row-2">
-        <div class="agung">
-            <h1></h1>
-            <img src="img/unique.png" alt="">
-       
-        </div>
-        <div class="agung">
-            <h1></h1>
-            <img src="img/pln.png" alt="">
-       
-        </div>
-        <div class="agung">
-            <h1></h1>
-            <img src="img/astra motor.png" alt="">
-       
-        </div>
-        <div class="agung">
-            <h1></h1>
-            <img src="img/atlas.png" alt="">
+</nav>
+<!-- Banner -->
+<div class="container-fluid banner text-left">
+   <h4 class="display-6">Welcome</h4>
+   <h3 class="display-1">Bali Job Finder</h3>
+   <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora voluptates corrupti quia possimus, sit beatae?</p>
+</div>
+<!-- Tips Section -->
+<div class="container-fluid tips" id="tips">
+  <div class="container-fluid">
+   <div class="row align-items-center">
+     <div class="col-md-6">
+      <a href="#" class="logo">
+        <img src="../bootsraps/img/Logo-tips.png" alt="" height="500px">
+      </a>
+     </div>
+     <div class="col-md-6">
+       <h3 class="display-4 text-center">Tips Melamar</h3>
+       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit est, esse facere provident aliquam numquam laboriosam fugiat totam, dolor, quia porro quos itaque alias distinctio perspiciatis deleniti voluptatem molestias magnam sed reiciendis rem eos quae. Error temporibus quaerat assumenda numquam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita repellendus rerum culpa facilis eum non? Molestiae asperiores minima, recusandae voluptatem dolores eveniet voluptatibus necessitatibus minus incidunt, id non error corporis.</p>
+     </div>
+    </div>
+  </div>
+</div>
+
+<!-- rekomendasi Perusahaan -->
+<div class="container">
+  <div class="judul">
+    <h3 class="section-title text-center">Rekomendasi Perusahaan Dari kami</h3>
+  </div>
+  <div class="row justify-content-center align-items-center">
+    <div class="col-md-2">
+     <div class="square">
+      <img src="img/blesing.png" alt="Logo 1" class="logo">
+     </div>
+    </div>
+    <div class="col-md-2">
+     <div class="square">
+      <img src="img/gosha.png" alt="Logo 2" class="logo">
+     </div>
+    </div>
+    <div class="col-md-2">
+     <div class="square">
+      <img src="img/astra motor.png" alt="Logo 3" class="logo">
+     </div>
+    </div>
+    <div class="col-md-2">
+     <div class="square">
+      <img src="img/gogo.png" alt="Logo 4" class="logo">
+     </div>
+    </div>
+    <div class="col-md-2">
+     <div class="square">
+      <img src="img/pln.png" alt="Logo 5" class="logo">
+     </div>
+    </div>
+   </div>
+  <div class="row justify-content-center align-items-center">
+   <div class="col-md-2">
+     <div class="square">
+      <img src="img/urban.png" alt="Logo 6" class="logo">
+     </div>
+   </div>
+   <div class="col-md-2">
+    <div class="square">
+     <img src="img/atlas.png" alt="Logo 7" class="logo">
+    </div>
+   </div>
+   <div class="col-md-2">
+    <div class="square">
+     <img src="img/keranjang.png" alt="Logo 8" class="logo">
+    </div>
+   </div>
+   <div class="col-md-2">
+    <div class="square">
+      <img src="img/unique.png" alt="Logo 9" class="logo">
+    </div>
+   </div>
+   <div class="col-md-2">
+    <div class="square">
+     <img src="img/khrisna.png" alt="Logo 10" class="logo">
+    </div>
+   </div>
+  </div>
+</div>
+<!-- perkerjaan terlaris -->
+<div class="container">
+  <div class="judul">
+    <h3 class="section-title text-center">Pekerjaan Yang Banyak Dicari</h3>
+  </div>
+  <div class="row justify-content-center align-items-center">
+    <div class="col-md-4">
+     <div class="square-new">
+      <img src="img/teknologi.jpg" alt="">
+      <div class="overlay">
+       <div class="overlay-content">
+        <h2>Teknologi</h2>
        </div>
-       <div class="agung">
-        <h1></h1>
-        <img src="img/blesing.png" alt="">
-   
+      </div>
+     </div>
     </div>
-    <div class="agung">
-        <h1></h1>
-        <img src="img/gogo.png" alt="">
-   
-    </div>
-    <div class="agung">
-        <h1></h1>
-        <img src="img/gosha.png" alt="">
-   
-    </div>
-    <div class="agung">
-        <h1></h1>
-        <img src="../pelamar/img/khrisna.png" alt="">
-   
-    </div>
-        </div>
-    </div>
-</div>
-</div>
-<!-- logo end -->
-
-<!-- perusahaan start -->
-<div class="bungkus1">
-  <div class="conten text-center">
-    <h1>Rekomendasi Perusahaan Dari Kami</h1>
-  </div>
-  <div class="row">
     <div class="col-md-4">
-      <div class="card h-100 position-relative">
-        <div class="overlay"></div>
-        <img src="img/teknologi.jpg" class="card-img-top" alt="...">
-        <div class="text-center text-white position-absolute w-100" style="top: 50%; transform: translateY(-50%); z-index: 2;">
-          <h3>Teknologi Informasi</h3>
+      <div class="square-new">
+       <img src="img/penjul.jpg" alt="">
+       <div class="overlay">
+        <div class="overlay-content">
+         <h2>Penjualan</h2>
         </div>
+       </div>
       </div>
     </div>
     <div class="col-md-4">
-      <div class="card h-100 position-relative">
-        <div class="overlay"></div>
-        <img src="img/penjul.jpg" class="card-img-top" alt="...">
-        <div class="text-center text-white position-absolute w-100" style="top: 50%; transform: translateY(-50%); z-index: 2;">
-          <h3>Penjualan</h3>
+     <div class="square-new">
+       <img src="img/pemasar.jpg" alt="">
+       <div class="overlay">
+        <div class="overlay-content">
+         <h2>Pemasaran</h2>
         </div>
-      </div>
-    </div>
-    <div class="col-md-4">
-      <div class="card h-100 position-relative">
-        <div class="overlay"></div>
-        <img src="img/pemasar.jpg" class="card-img-top" alt="...">
-        <div class="text-center text-white position-absolute w-100" style="top: 50%; transform: translateY(-50%); z-index: 2;">
-          <h3>Pemasaran</h3>
-        </div>
+       </div>
       </div>
     </div>
   </div>
